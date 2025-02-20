797085e7147a5639b9a2f367acb1d27214dec14f
CarlosG18 <c.gabriel.abc18@gmail.com>
2025-02-20 17:54:12 -0300

    feat: front-> iniciando a tela de dashboard

--- evolutone/urls.py
+++ evolutone/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('apps.users.urls'), name='users'),
-    path('main/', include('apps.main.urls'), name='main'),
+    path('', include('apps.main.urls'), name='main'),
 ]
