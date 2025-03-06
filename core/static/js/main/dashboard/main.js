5c2b6e91eadca90d592f4451a882109d27d6b720
CarlosG18 <c.gabriel.abc18@gmail.com>
2025-03-06 11:26:29 -0300

    feat: front-> iniciando a navbar

--- core/static/js/main/dashboard/main.js
+++ core/static/js/main/dashboard/main.js
@@ -1,4 +1,6 @@
 const button_barra_lateral = document.getElementById('btn-barra-lateral')
+const dashboard_body = document.getElementById('dashboard-body')
+const nav_search = document.getElementById('nav-search')
 
 button_barra_lateral.addEventListener('click', ativar_animacao)
 
@@ -6,5 +8,7 @@ function ativar_animacao(){
     const navbar = document.getElementById("navbar")
 
     navbar.classList.add('animation');
+    dashboard_body.classList.add('animation-body');
+    nav_search.classList.add('animation-body');
 }
 
